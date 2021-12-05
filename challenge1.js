772b2b5cfcc53b332e4066436ab08835c30774ad
Tomeu <cabotvalloritomeu@gmail.com>
2021-12-05 08:07:05 -0800

    ✨challenge #1

---
+++ challenge1.js
@@ -0,0 +1,29 @@
+/*
+Considera una lista/array de ovejas. Cada oveja tiene un nombre y un color. Haz una función que devuelva una lista con todas las ovejas que sean de color rojo y que además su nombre contenga tanto las letras n Y a, sin importar el orden, las mayúsculas o espacios.
+
+Por ejemplo, si tenemos las ovejas:
+```
+const ovejas = [
+  { name: 'Noa', color: 'azul' },
+  { name: 'Euge', color: 'rojo' },
+  { name: 'Navidad', color: 'rojo' },
+  { name: 'Ki Na Ma', color: 'rojo'}
+]
+```
+
+Al ejecutar el método debería devolver lo siguiente:
+
+const ovejasFiltradas = contarOvejas(ovejas)\
+```
+console.log(ovejasFiltradas)
+
+// [{ name: 'Navidad', color: 'rojo' },
+//  { name: 'Ki Na Ma', color: 'rojo' }]
+```
+*/
+
+module.exports = function contarOvejas(ovejas) {
+	return ovejas.filter(
+		(oveja) => oveja.color === "rojo" && /(?=.*n)(?=.*a)/i.test(oveja.name)
+	);
+};
